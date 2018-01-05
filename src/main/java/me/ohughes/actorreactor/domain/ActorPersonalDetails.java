9b628978e4bacbfd7f62c8f90ac7629541158429
Ollie Hughes <ohughes@pivotal.io>
2018-01-05 18:43:12 +0000

    Extract year of birth for actors

--- src/main/java/me/ohughes/actorreactor/domain/ActorPersonalDetails.java
+++ src/main/java/me/ohughes/actorreactor/domain/ActorPersonalDetails.java
@@ -1,12 +1,14 @@
 package me.ohughes.actorreactor.domain;
 
 import java.time.LocalDate;
+import java.time.Year;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ActorPersonalDetails {
-	private String firstNames;
-	private String lastNames;
-	private LocalDate yearOfBirth;
+	private String fullName;
+	private Year yearOfBirth;
 }
--- src/main/java/me/ohughes/actorreactor/functions/ExtractActorFunction.java
+++ src/main/java/me/ohughes/actorreactor/functions/ExtractActorFunction.java
@@ -1,14 +1,31 @@
 package me.ohughes.actorreactor.functions;
 
+import java.time.Year;
 import java.util.function.Function;
 
+import me.ohughes.actorreactor.domain.ActorPersonalDetails;
 import reactor.core.publisher.Flux;
 
-public class ExtractActorFunction implements Function<Flux<String>, Flux<String>> {
+import static java.lang.Integer.parseInt;
+
+public class ExtractActorFunction implements Function<Flux<String>, Flux<ActorPersonalDetails>> {
 	@Override
-	public  Flux<String> apply(Flux<String> fileLinesFlux) {
+	public  Flux<ActorPersonalDetails> apply(Flux<String> fileLinesFlux) {
 		return fileLinesFlux.map(s -> s.split("\t"))
-							.filter(a -> a.length > 1)
-							.map(a -> a[1]);
+							.filter(a -> a.length > 2)
+							.filter(a -> isNumber(a[2]))
+							.map(a -> ActorPersonalDetails.builder()
+									.fullName(a[1])
+									.yearOfBirth(Year.of(parseInt(a[2])))
+									.build());
+	}
+
+	private boolean isNumber(String s) {
+		try {
+			parseInt(s);
+			return true;
+		} catch (NumberFormatException nfe){
+			return true;
+		}
 	}
 }
